915a100a700897db023a00118268367eab7e9fa4
Superkatten <Superkatten@10.16.117.25>
2012-08-21 14:24:05 +0200

    Payments implemented

--- app/Model/Payment.php
+++ app/Model/Payment.php
@@ -7,6 +7,30 @@
  */
 class Payment extends AppModel {
 	public $belongsTo = array('User', 'Crew');
+	
+	public $validate = array(
+		'amount' => array(
+			'required1' => array(
+				'rule' => array('notEmpty', 'numeric'),
+				'message' => 'Amount is invalid'
+			),
+			'required2' => array(
+				'rule' => 'positiveInteger',
+				'message' => 'Amount is invalid'
+			),
+		)
+	);
+	
+	public function positiveInteger($check){
+		if(is_numeric($check['amount']) && $check['amount'] > 0 && $check['amount'] <= 999){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	
 
 }
 
