b001493d11c97e88ee755a5b0fb0d77a213e3530
Jens <Jens@192.168.2.7>
2013-01-23 16:17:32 +0100

    Upload plugin, Bootstrap, FontAwesome added. jQuery+jQueryUI updated.

--- app/webroot/js/admin/user_lookup.js
+++ app/webroot/js/admin/user_lookup.js
@@ -1,55 +1,48 @@
-$(document).ready(function(){
+$(function() {
 
 	$user_lookup = $('#user_lookup');
-	$input = $user_lookup.find('input');
+	$input = $user_lookup.find('#user_lookup_input');
 	var urlLookup = $user_lookup.find('#urlLookup a').attr('href');
 	var urlRedirect = $user_lookup.find('#urlRedirect a').attr('href');
 
 	$('#user_lookup input').autocomplete({
+		minLength: 2,
 		source: function( request, response ) {
-			$.ajax({
-				url: urlLookup,
-				//				url: "http://ws.geonames.org/searchJSON",
-				dataType: "json",
-				type : 'POST',
-				data: {
-					featureClass: "P",
-					style: "full",
-					maxRows: 12,
-					search_startsWith: request.term
-				},
-				success: function( data ) {
-					response( $.map( data.users, function( item ) {
-						return {
-							label: item.User.name,
-							id_number: item.User.id_number,
-							email: item.User.email,
-							id: item.User.id,
-							value: item.User.id_number
-						}
-					}));
+		$.ajax({
+			url: urlLookup,
+			dataType: "json",
+			type : 'POST',
+			data: {
+			maxRows: 12,
+			search_startsWith: request.term
+			},
+			success: function( data ) {
+			response( $.map( data.users, function( item ) {
+				return {
+				label: item.User.name,
+				id_number: item.User.id_number,
+				email: item.User.email,
+				id: item.User.id,
+				value: item.User.id_number
 				}
+				}));
+			}
 			});
 		},
-		minLength : 3,
 		selectFirst : true,
 		select: function( event, ui ) {
-			if(ui.item){
-				document.location = urlRedirect + '/' + ui.item.id;
-			}
-		},
-		open: function() {
-			$( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-		},
-		close: function() {
-			$( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+		if(ui.item){
+		document.location = urlRedirect + '/' + ui.item.id;
 		}
-	}).data("autocomplete")._renderItem = function(ul, item) {
+		return false;
+		}
+		})
+	.data( "ui-autocomplete" )._renderItem = function( ul, item ) {
 		return $("<li></li>").data("item.autocomplete", item).append(
-		"<a>"+
-			"<strong>" + item.label + "</strong>"+
-			"<br />" +
-		"<small>" + item.id_number + "<br />" + item.email + "</small>" +
+			"<a>" +
+			"<strong>" + item.label + "</strong>" +
+			//			"<br />" +
+			//			"<small>" + item.id_number + "<br />" + item.email + "</small>" +
 			"</a>")
 		.appendTo(ul);
 	};
