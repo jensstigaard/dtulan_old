ae4049751da99e0d5ba91d638598852b8cb91657
Jens <Jens@192.168.2.7>
2013-04-10 18:01:30 +0200

    Using prefix now (admin)

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -43,7 +43,7 @@ class UsersController extends AppController {
 		return false;
 	}
 
-	public function index() {
+	public function admin_index() {
 
 		$this->paginate = array(
 			 'User' => array(
